21ee4dd40df67837f158d36a46495dbac13f5075
mxro <mxro@nowhere.com>
2015-08-07 08:49:12 +1000

    Implemting JavaScript server-side procedures Autocommit Fri Aug 07 08:49:12 EST 2015

--- src/test/java/delight/nashornsandbox/tests/TestAllowAccess.java
+++ src/test/java/delight/nashornsandbox/tests/TestAllowAccess.java
@@ -1,5 +1,16 @@
 package delight.nashornsandbox.tests;
 
+import delight.nashornsandbox.NashornSandbox;
+import delight.nashornsandbox.NashornSandboxes;
+import java.io.File;
+import org.junit.Test;
+
 @SuppressWarnings("all")
 public class TestAllowAccess {
+  @Test
+  public void test_file() {
+    final NashornSandbox sandbox = NashornSandboxes.create();
+    sandbox.allow(File.class);
+    sandbox.eval("var File = Java.type(\"java.io.File\"); File;");
+  }
 }
