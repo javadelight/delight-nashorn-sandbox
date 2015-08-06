b1c958ba7a3a263295f8d1faee6d630dacf4214b
mxro <mxro@nowhere.com>
2015-08-07 08:13:12 +1000

    Implemting JavaScript server-side procedures Autocommit Fri Aug 07 08:13:12 EST 2015

--- src/main/java/delight/nashornsandbox/internal/SandboxClassFilter.java
+++ src/main/java/delight/nashornsandbox/internal/SandboxClassFilter.java
@@ -1,5 +1,11 @@
 package delight.nashornsandbox.internal;
 
+import jdk.nashorn.api.scripting.ClassFilter;
+
 @SuppressWarnings("all")
-public class SandboxClassFilter {
+public class SandboxClassFilter implements ClassFilter {
+  @Override
+  public boolean exposeToScripts(final String arg0) {
+    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  }
 }
--- src/main/xtend/delight/nashornsandbox/internal/SandboxClassFilter.xtend
+++ src/main/xtend/delight/nashornsandbox/internal/SandboxClassFilter.xtend
@@ -1,5 +1,10 @@
 package delight.nashornsandbox.internal
 
-class SandboxClassFilter  {
-	
+import jdk.nashorn.api.scripting.ClassFilter;
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
+
+class SandboxClassFilter implements ClassFilter {
+	override exposeToScripts(String arg0) {
+		throw new UnsupportedOperationException("TODO: auto-generated method stub")
+	}
 }
