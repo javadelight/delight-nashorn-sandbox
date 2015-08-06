d22d93541b6c1f38fb9dfb39c8d422992d55f6dd
mxro <mxro@nowhere.com>
2015-08-07 08:29:12 +1000

    Implemting JavaScript server-side procedures Autocommit Fri Aug 07 08:29:12 EST 2015

--- src/main/java/delight/nashornsandbox/internal/NashornSandboxImpl.java
+++ src/main/java/delight/nashornsandbox/internal/NashornSandboxImpl.java
@@ -1,5 +1,17 @@
 package delight.nashornsandbox.internal;
 
+import delight.nashornsandbox.NashornSandbox;
+import javax.script.ScriptEngine;
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
+
 @SuppressWarnings("all")
-public class NashornSandboxImpl {
+public class NashornSandboxImpl implements NashornSandbox {
+  public ScriptEngine createScriptEngine() {
+    ScriptEngine _xblockexpression = null;
+    {
+      final NashornScriptEngineFactory factory = new NashornScriptEngineFactory();
+      _xblockexpression = factory.getScriptEngine();
+    }
+    return _xblockexpression;
+  }
 }
--- src/main/xtend/delight/nashornsandbox/internal/NashornSandboxImpl.xtend
+++ src/main/xtend/delight/nashornsandbox/internal/NashornSandboxImpl.xtend
@@ -1,5 +1,18 @@
 package delight.nashornsandbox.internal
 
-class NashornSandboxImpl {
+import delight.nashornsandbox.NashornSandbox
+import javax.script.ScriptEngine
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory
+
+class NashornSandboxImpl implements NashornSandbox {
+	
+	def ScriptEngine createScriptEngine() {
+		/*
+		 * If eclipse shows an error here, see http://stackoverflow.com/a/10642163/270662
+		 */
+		val NashornScriptEngineFactory factory = new NashornScriptEngineFactory();
+		
+		factory.getScriptEngine();
+	}
 	
 }
