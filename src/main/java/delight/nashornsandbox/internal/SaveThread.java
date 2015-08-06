db15f874f710629471220364e9eec74f701837f2
mxro <mxro@nowhere.com>
2015-08-07 09:31:12 +1000

    Implemting JavaScript server-side procedures Autocommit Fri Aug 07 09:31:12 EST 2015

--- src/main/java/delight/nashornsandbox/internal/SaveThread.java
+++ src/main/java/delight/nashornsandbox/internal/SaveThread.java
@@ -1,5 +1,11 @@
 package delight.nashornsandbox.internal;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+
 @SuppressWarnings("all")
 public class SaveThread {
+  public void test() {
+    final ThreadMXBean bean = ManagementFactory.getThreadMXBean();
+  }
 }
--- src/main/xtend/delight/nashornsandbox/internal/SaveThread.xtend
+++ src/main/xtend/delight/nashornsandbox/internal/SaveThread.xtend
@@ -1,8 +1,12 @@
 package delight.nashornsandbox.internal
 
+import java.lang.management.ManagementFactory
+
 class SaveThread  {
 	
-	
+	def test() {
+		val bean = ManagementFactory.getThreadMXBean();
+	}
 
 	
 }
