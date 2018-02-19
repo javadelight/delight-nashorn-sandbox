b2c771eae1d6b4f016a62df05b338c5982a8541e
Max Rohde <mxro@nowhere.com>
2018-02-20 08:26:32 +1100

    Adding test for issue #40.

--- src/main/java/delight/nashornsandbox/NashornSandboxes.java
+++ src/main/java/delight/nashornsandbox/NashornSandboxes.java
@@ -14,16 +14,25 @@ import delight.nashornsandbox.internal.NashornSandboxImpl;
  */
 @SuppressWarnings("all")
 public class NashornSandboxes {
-
+	
+	/**
+	 * <p>Creates a new sandbox instance.
+	 * 
+	 * @return The newly created sandbox instance.
+	 */
 	public static NashornSandbox create() {
 		return new NashornSandboxImpl();
 	}
 	
 	/**
 	 * <p>Create a sandbox while supplying arguments for the engine such as '--no-java'.
-	 * 
-	 * @param args
-	 * @return
+	 * <p>More information on available parameters can be found in the following:
+	 * <ul>
+	 * <li><a href='http://hg.openjdk.java.net/jdk8/jdk8/nashorn/file/tip/docs/DEVELOPER_README'>Nashorn DEVELOPER README</a></li>
+	 * <li><a href='https://github.com/JetBrains/jdk8u_nashorn/blob/master/src/jdk/nashorn/internal/runtime/resources/Options.properties'>Source Code Options.properties</a></li>
+	 * <ul>
+	 * @param args Options for initializing the Nashorn engine.
+	 * @return A newly created sandbox instance.
 	 */
 	public static NashornSandbox create(String... args) {
 		return new NashornSandboxImpl(args);
