b9f3cc9bee51dbd29d861b1e5546ade4ff7937d1
Max Rohde <mxro@nowhere.com>
2018-05-04 15:06:26 +1000

    Adding unit test for #56.

---
+++ src/test/java/delight/nashornsandbox/TestIssue56.java
@@ -0,0 +1,29 @@
+package delight.nashornsandbox;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.script.ScriptException;
+
+import org.junit.Test;
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
+
+/**
+ * See https://github.com/javadelight/delight-nashorn-sandbox/issues/57
+ * @author Max
+ *
+ */
+public class TestIssue56 {
+
+	@Test
+	public void testMapReduce() throws ScriptException {
+		String script = "[1,2,3,4].map(function(n){return n+1}).reduce(function(prev,cur){return prev+cur}, 0)";
+		String[] NASHORN_ARGS = { "--no-java", "--no-syntax-extensions" };
+
+		Double nashornResult = (Double) new NashornScriptEngineFactory().getScriptEngine(NASHORN_ARGS).eval(script);
+		assertEquals(14, nashornResult.intValue());
+
+		Double sandboxResults = (Double) NashornSandboxes.create(NASHORN_ARGS).eval(script);
+		assertEquals(14, sandboxResults.intValue());
+	}
+
+}
