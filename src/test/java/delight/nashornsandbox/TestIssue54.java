32fe63a240a9468b15efb8fbbc08e0a7be5ee783
Max Rohde <mxro@nowhere.com>
2018-05-04 15:20:37 +1000

    Adding test for issue #54.

---
+++ src/test/java/delight/nashornsandbox/TestIssue54.java
@@ -0,0 +1,32 @@
+package delight.nashornsandbox;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.script.ScriptException;
+
+import org.junit.Test;
+
+import delight.nashornsandbox.exceptions.ScriptCPUAbuseException;
+
+public class TestIssue54 {
+
+	@Test
+	public void test_valid() throws ScriptCPUAbuseException, ScriptException {
+
+		String js = "var x = 1;\nwhile (true) { }\n";
+
+		NashornSandbox sandbox = NashornSandboxes.create();
+		sandbox.setMaxCPUTime(100);
+		sandbox.setMaxMemory(1000 * 1000);
+		sandbox.allowNoBraces(false);
+		sandbox.disallowAllClasses();
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		sandbox.setExecutor(executor);
+		sandbox.eval(js);
+
+		sandbox.getExecutor().shutdown();
+
+	}
+	
+}
