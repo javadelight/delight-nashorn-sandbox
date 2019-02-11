420aaff23553d2d460ace756a217da83ec9c15c9
Srinivasa Chintalapati <KH2363@kitspl.com>
2019-02-11 11:17:12 +0530

    [BUG_FIX]: Executor thread not set after <some value> ms - intermittently #75
    > Fixed issue my adding additional 100ms delay to get the thread monitor instance

--- src/main/java/delight/nashornsandbox/internal/ThreadMonitor.java
+++ src/main/java/delight/nashornsandbox/internal/ThreadMonitor.java
@@ -84,7 +84,7 @@ public class ThreadMonitor {
 			// wait, for threadToMonitor to be set in JS evaluator thread
 			synchronized (monitor) {
 				if (threadToMonitor == null) {
-					monitor.wait(maxCPUTime / MILI_TO_NANO);
+					monitor.wait((maxCPUTime + 100) / MILI_TO_NANO);
 				}
 			}
 			if (threadToMonitor == null) {
