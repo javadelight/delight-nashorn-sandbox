35c5ea276dfa45a131de0f3101c7cd20fb4c4d49
Max Rohde <1448524+mxro@users.noreply.github.com>
2023-05-21 12:26:41 +1000

    Adding protecting against regex attack

---
+++ src/main/java/delight/nashornsandbox/internal/SecureInterruptibleCharSequence.java
@@ -0,0 +1,42 @@
+package delight.nashornsandbox.internal;
+
+import delight.nashornsandbox.exceptions.ScriptCPUAbuseException;
+
+public class SecureInterruptibleCharSequence  implements CharSequence {
+   final CharSequence inner;
+    final ThreadLocal<Integer> matchCount;
+
+		public SecureInterruptibleCharSequence(CharSequence inner, ThreadLocal<Integer> matchCount) {
+			super();
+			this.inner = inner;
+      this.matchCount = matchCount;
+		}
+
+		@Override
+		public char charAt(int index) {
+			matchCount.set(matchCount.get()+1);
+			if (matchCount.get() > 100000){
+				Thread thread = Thread.currentThread();
+				thread.interrupt();
+			}
+			if (Thread.currentThread().isInterrupted()) {
+				throw new ScriptCPUAbuseException("Regular expression running for too many iterations.", true, null);
+			}
+			return inner.charAt(index);
+		}
+
+		@Override
+		public int length() {
+			return inner.length();
+		}
+
+		@Override
+		public CharSequence subSequence(int start, int end) {
+			return new SecureInterruptibleCharSequence(inner.subSequence(start, end), matchCount);
+		}
+
+		@Override
+		public String toString() {
+			return inner.toString();
+		}
+}
