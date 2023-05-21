83be5b16b63b6793bdaa841433ed9d48c69735c8
Max Rohde <1448524+mxro@users.noreply.github.com>
2023-05-21 12:33:23 +1000

    Increasing max operations on regex

--- src/main/java/delight/nashornsandbox/internal/SecureInterruptibleCharSequence.java
+++ src/main/java/delight/nashornsandbox/internal/SecureInterruptibleCharSequence.java
@@ -15,7 +15,7 @@ public class SecureInterruptibleCharSequence  implements CharSequence {
 		@Override
 		public char charAt(int index) {
 			matchCount.set(matchCount.get()+1);
-			if (matchCount.get() > 100000){
+			if (matchCount.get() > 5000000){
 				Thread thread = Thread.currentThread();
 				thread.interrupt();
 			}
