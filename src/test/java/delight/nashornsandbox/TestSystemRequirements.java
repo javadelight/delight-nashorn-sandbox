a8d0ba3f2c0816703eca8afe3cadab9b87e8c820
Max Rohde <1448524+mxro@users.noreply.github.com>
2023-05-27 07:10:25 +1000

    Fixing build statement and extending test fail message

--- src/test/java/delight/nashornsandbox/TestSystemRequirements.java
+++ src/test/java/delight/nashornsandbox/TestSystemRequirements.java
@@ -9,7 +9,8 @@ public class TestSystemRequirements {
   @Test
   public void test_cpu() {  
     int processors = Runtime.getRuntime().availableProcessors();
-    assertTrue("Unit tests likely to fail since they are run on a low-spec machine. Try to run on a machine with at least 2 CPU cores", processors > 1);
+    assertTrue("Unit tests likely to fail since they are run on a low-spec machine. Try to run on a machine with at least 2 CPU cores.\n"+
+    "Processors detected: "+processors, processors > 1);
   }
 
   @Test
@@ -17,7 +18,9 @@ public class TestSystemRequirements {
     long maxMemory = Runtime.getRuntime().maxMemory();
     assertTrue("Unit test likely to fail. Run in an environment where the JVM has access to at least 2 GB max memory.\n"+
       "Max heap memory detected "+
-      maxMemory, maxMemory > 1_320_327_936);
+      maxMemory + "\n"+
+      "Also ensure correct JVM parameters set when running tests, e.g. -Drun.jvmArguments=\"-Xmx4048m -Xms1024m\"",
+      maxMemory > 1_320_327_936);
   }
   
 }
