1940ba2b248b58466a7fd0e993126b5f838dd35a
Max Rohde <1448524+mxro@users.noreply.github.com>
2025-01-05 13:41:02 +1100

    Fixing formatting and indentation

--- src/main/java/delight/nashornsandbox/internal/JsEvaluator.java
+++ src/main/java/delight/nashornsandbox/internal/JsEvaluator.java
@@ -6,10 +6,12 @@ import javax.script.ScriptEngine;
 
 /**
  * The JavaScript evaluator. It is designed to run Nashorn engine in separate
- * thread (using provided {@link ExecutorService}), to allow limit cpu time 
- * consumed. 
+ * thread (using provided {@link ExecutorService}), to allow limit cpu time
+ * consumed.
  *
- * <p>Created on 2017.11.22</p>
+ * <p>
+ * Created on 2017.11.22
+ * </p>
  *
  * @author <a href="mailto:marcin.golebski@verbis.pl">Marcin Golebski</a>
  * @version $Id$
@@ -17,12 +19,13 @@ import javax.script.ScriptEngine;
 class JsEvaluator implements Runnable {
   private final ThreadMonitor threadMonitor;
   private final ScriptEngine scriptEngine;
-    
+
   private Object result = null;
   private Exception exception = null;
   private final ScriptEngineOperation operation;
 
-  JsEvaluator(final ScriptEngine scriptEngine, final long maxCPUTime, final long maxMemory, ScriptEngineOperation operation) {
+  JsEvaluator(final ScriptEngine scriptEngine, final long maxCPUTime, final long maxMemory,
+      ScriptEngineOperation operation) {
     this.scriptEngine = scriptEngine;
     this.threadMonitor = new ThreadMonitor(maxCPUTime, maxMemory);
     this.operation = operation;
@@ -35,18 +38,18 @@ class JsEvaluator implements Runnable {
   boolean isCPULimitExceeded() {
     return threadMonitor.isCPULimitExceeded();
   }
-  
+
   boolean isMemoryLimitExceeded() {
     return threadMonitor.isMemoryLimitExceeded();
   }
 
   /**
-   * Enter the monitor method. It should be called from main thread. 
+   * Enter the monitor method. It should be called from main thread.
    */
   void runMonitor() {
     threadMonitor.run();
   }
-    
+
   @Override
   public void run() {
     try {
@@ -54,18 +57,15 @@ class JsEvaluator implements Runnable {
       if (registered) {
         result = operation.executeScriptEngineOperation(scriptEngine);
       }
-    }
-    catch (final RuntimeException e) {
+    } catch (final RuntimeException e) {
       // InterruptedException means script was successfully interrupted,
       // so no exception should be propagated
-      if(!(e.getCause() instanceof InterruptedException)) {
+      if (!(e.getCause() instanceof InterruptedException)) {
         exception = e;
       }
-    }
-    catch (final Exception e) {
+    } catch (final Exception e) {
       exception = e;
-    } 
-    finally {
+    } finally {
       threadMonitor.scriptFinished();
       threadMonitor.stopMonitor();
     }
@@ -74,7 +74,7 @@ class JsEvaluator implements Runnable {
   Exception getException() {
     return exception;
   }
-  
+
   Object getResult() {
     return result;
   }
